d635e0ad7a2435b8aa624a1e2780c61952e93d66
William Angulo <wmavalle@gmail.com>
2022-03-19 20:49:54 -0500

    Swagger config almost finished

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -2,6 +2,10 @@ package com.example.demo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -13,6 +17,19 @@ public class SwaggerConfig {
     @Bean
     public Docket studentApi(){
         return  new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(getApiInfo())
+                .select()
+                .paths(PathSelectors.any())
+                .build();
+    }
 
+    private ApiInfo getApiInfo(){
+        Contact contact = new Contact("william","http://william.com","wmavalle@gmail.com");
+        return new ApiInfoBuilder()
+                .title("Students API")
+                .description("Swagger UI documentation")
+                .version("1.0")
+                .contact(contact)
+                .build();
     }
 }
